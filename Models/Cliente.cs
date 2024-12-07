176f6a0ab22279fe9344648352920a1e2388c8e2
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-12-07 13:32:24 -0300

    Update Cliente.cs
    Migrations Messages

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace IHCATCadClientes.Models
 {
@@ -6,68 +6,68 @@ namespace IHCATCadClientes.Models
     {
         [Key]
         public int id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Informe seu Nome")]
         [Display(Name = "Nome")]
         [StringLength(100)]
         public string Nome { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe sua Data de Nascimento")]
         [DataType(DataType.Date)]
         [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Informe seu RG")]
         [Display(Name = "RG")]
         [StringLength(20)]
         public string RG { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe seu CPF")]
         [Display(Name = "CPF")]
         [StringLength(14)]
         public string CPF { get; set; } = string.Empty;
 
         // Endereço residencial
-        [Required]
+        [Required(ErrorMessage = "Informe seu Logradouro (Endereço)")]
         [Display(Name = "Logradouro")]
         [StringLength(200)]
         public string Logradouro { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe seu Bairro")]
         [Display(Name = "Bairro")]
         [StringLength(100)]
         public string Bairro { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe sua Cidade")]
         [Display(Name = "Cidade")]
         [StringLength(100)]
         public string Cidade { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe o Complemento")]
         [Display(Name = "Complemento")]
         [StringLength(100)]
         public string Complemento { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe sua Unidade Federativa")]
         [Display(Name = "UF")]
         [StringLength(2)]
         public string UF { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe seu CEP")]
         [Display(Name = "CEP")]
         [StringLength(9)]
         public string CEP { get; set; } = string.Empty;
         // fim do Endereço Residencial
 
-        [Required]
+        [Required(ErrorMessage = "Informe seu Estado Civil")]
         [Display(Name = "Estado Civil")]
         public EstadoCivil EstadoCivil { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Informe o Nome do seu Pai")]
         [Display(Name = "Nome do Pai")]
         [StringLength(100)]
         public string NomePai { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe o Nome de sua Mãe")]
         [Display(Name = "Nome da Mãe")]
         [StringLength(100)]
         public string NomeMae { get; set; } = string.Empty;
