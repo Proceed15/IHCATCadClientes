bbdd1cecf68b5079b87af500f6df30ecdd0a7b9f
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-12-07 00:24:04 -0300

    Add files via upload

---
+++ IHCATCadClientes/Data/IHCATCadClientesContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using IHCATCadClientes.Models;
+
+namespace IHCATCadClientes.Data
+{
+    public class IHCATCadClientesContext : DbContext
+    {
+        public IHCATCadClientesContext (DbContextOptions<IHCATCadClientesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<IHCATCadClientes.Models.Cliente> Cliente { get; set; } = default!;
+
+    }
+}
---
+++ IHCATCadClientes/Migrations/20241207015211_Initial.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IHCATCadClientes.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RG = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Logradouro = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Complemento = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    CEP = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    EstadoCivil = table.Column<int>(type: "int", nullable: false),
+                    NomePai = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    NomeMae = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
