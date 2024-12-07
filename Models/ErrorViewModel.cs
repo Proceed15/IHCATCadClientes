387b7b9d8c9fd728d52e4625d4a78514b485f075
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-12-07 01:34:37 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IHCATCadClientes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
